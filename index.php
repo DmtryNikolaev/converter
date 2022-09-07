0e1dc69a4b99c9d880a464bd8e6a23ab47446c6c
DimonNikolaev <dmitriy.nikolaev1337@gmail.com>
2022-09-07 21:24:09 +0500

    Форма для конвертации валют

--- index.php
+++ index.php
@@ -1,19 +1,33 @@
+<?php require_once('src/views/app.html'); ?>
+
 <?php
+$api_key = 'c12708446dcc574db47b3ff6908b2650';
+$result = '';
+if (isset($_POST['conversion'])) {
+    $request = $_POST['conversion'];
+    [$count, $crypto,, $currency] = explode(' ', $request);
+    $crypto = strtoupper($crypto);
+    $currency = strtoupper($currency);
+    $url = "https://min-api.cryptocompare.com/data/pricemulti?fsyms=$crypto&tsyms=$currency&api_key=$api_key";
+    $minute = 60;
+    $isExisted = apcu_exists($crypto);
 
-require_once './vendor/autoload.php';
+    if (!$isExisted) {
+        $cost = json_decode(file_get_contents($url), true)[$crypto][$currency];
 
-$request = $_SERVER['REQUEST_URI'];
-$loader = new \Twig\Loader\FilesystemLoader('./src/views');
-$twig = new \Twig\Environment($loader);
-$url = 'http://api.coinlayer.com/api/live?access_key=c12708446dcc574db47b3ff6908b2650&symbols=BTC,ETH,AIDOC,AIB,AGI,AE,ADZ,ADX,ADL,ADCN,ADA,USD';
+        apcu_store($crypto, [$crypto => $cost], $minute);
+    }
 
-switch ($request) {
-    case '/':
-        echo $twig->render('form.html');
-        break;
-    case '/listOfCurrency':
-        $listOfCurrency = (array)((array) json_decode(file_get_contents($url)))['rates'];
 
-        echo $twig->render('curency.html', compact('listOfCurrency'));
-        break;
+    $resultOfexpression = (int) apcu_fetch($crypto)[$crypto] * (int) $count;
+    $result = "$resultOfexpression $currency";
 }
+
+?>
+<div class="container mt-3">
+    <form method="POST" class="col-6 p-0">
+        <input type="text" name="conversion" class="form-control mb-2" placeholder="2 btc in usd">
+        <input type="submit" value="Расчет" class="btn btn-primary w-100">
+        <span>Расчет: <b><?= $result; ?></b></span>
+    </form>
+</div>
