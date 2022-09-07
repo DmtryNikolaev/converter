1cc24193e1a9fef36167b742c61ae739e0556d91
DimonNikolaev <dmitriy.nikolaev1337@gmail.com>
2022-09-08 01:59:54 +0500

    форматирование строки запроса

--- curency.php
+++ curency.php
@@ -2,9 +2,7 @@
 
 <?php
 $api_key = 'c12708446dcc574db47b3ff6908b2650';
-$url = "https://min-api.cryptocompare.com/data/pricemulti
-    ?fsyms=BTC,ETH,DOG,USDC,USDT,MATIC,FTT,FLOW,EGLD,BTTOLD
-    &tsyms=RUB,USD&api_key=$api_key";
+$url = "https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH,DOG,USDC,USDT,MATIC,FTT,FLOW,EGLD,BTTOLD&tsyms=RUB,USD&api_key=$api_key";
 $list = json_decode(file_get_contents($url), true);
 ?>
 
