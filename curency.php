42e4d872fd51584c8949d99b3b721b0bc9350f08
DimonNikolaev <dmitriy.nikolaev1337@gmail.com>
2022-09-08 00:33:49 +0500

    Вывод списка криптовалют

---
+++ curency.php
@@ -0,0 +1,17 @@
+<?php require_once('./app.html'); ?>
+
+<?php
+$api_key = 'c12708446dcc574db47b3ff6908b2650';
+$url = "https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH,DOG,USDC,USDT,MATIC,FTT,FLOW,EGLD,BTTOLD&tsyms=RUB,USD&api_key=$api_key";
+$list = json_decode(file_get_contents($url), true);
+?>
+
+
+<ul class="list-group">
+
+    <?php foreach ($list as $key => $value) { ?>
+        <li class="list-group-item">
+            <?php echo $key . ' ' . $value['RUB'] . ' RUB'; ?>/ <?php echo $value['USD'] . ' USD'; ?>
+        </li>
+    <?php } ?>
+</ul>
