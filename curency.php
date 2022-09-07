321f4bdf2e10d621eec4375e048d648094706ca7
DimonNikolaev <dmitriy.nikolaev1337@gmail.com>
2022-09-08 01:14:10 +0500

    Установка phpcodesniffer, форматирование файлов по станд

--- curency.php
+++ curency.php
@@ -2,7 +2,9 @@
 
 <?php
 $api_key = 'c12708446dcc574db47b3ff6908b2650';
-$url = "https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH,DOG,USDC,USDT,MATIC,FTT,FLOW,EGLD,BTTOLD&tsyms=RUB,USD&api_key=$api_key";
+$url = "https://min-api.cryptocompare.com/data/pricemulti
+    ?fsyms=BTC,ETH,DOG,USDC,USDT,MATIC,FTT,FLOW,EGLD,BTTOLD
+    &tsyms=RUB,USD&api_key=$api_key";
 $list = json_decode(file_get_contents($url), true);
 ?>
 
@@ -10,8 +12,8 @@ $list = json_decode(file_get_contents($url), true);
 <ul class="list-group">
 
     <?php foreach ($list as $key => $value) { ?>
-        <li class="list-group-item">
-            <?php echo $key . ' ' . $value['RUB'] . ' RUB'; ?>/ <?php echo $value['USD'] . ' USD'; ?>
-        </li>
+    <li class="list-group-item">
+        <?php echo $key . ' ' . $value['RUB'] . ' RUB'; ?>/ <?php echo $value['USD'] . ' USD'; ?>
+    </li>
     <?php } ?>
 </ul>
